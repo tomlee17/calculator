cd8f8185ec3bcc5deb8264a94fa21eb4d12cf744
TomL <youtom.lee17@gmail.com>
2021-11-27 21:15:08 +0800

    Added all-clear, backspace, percent, float features Added keyboard input support

--- calc.js
+++ calc.js
@@ -1,19 +1,15 @@
 function add(a, b) {
     return a + b;
 }
-
 function subtract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
     return a / b;
 }
-
 function operate(operator, a, b) {
     return operator(a, b);
 }
@@ -39,24 +35,33 @@ const operatorFunctions = {
     subtract,
     add,
 }
+let refreshNumCheck;
 
 numbers.forEach(function (number) {
     number.addEventListener('click', function () {
-        if (display.textContent === '0') {
-            display.textContent = number.textContent;
+        if (display.textContent === '0' || display.textContent === 'ERROR' || refreshNumCheck === display.textContent) {
+            display.textContent = this.textContent;
         }
-        else display.textContent += number.textContent;
+        else display.textContent += this.textContent;
     });
 });
 
+
+const float = document.querySelector('#float');
+float.addEventListener('click', function () {
+    if (display.textContent.split(/[-+*/]/).slice(-2).every(num => num.includes('.'))) return;
+    if (display.textContent === 'ERROR' || refreshNumCheck === display.textContent) {
+        display.textContent = this.textContent;
+    }
+    else display.textContent += this.textContent;
+})
+
 operations.forEach(function (operation) {
-    operation.addEventListener('click', function (e) {
+    operation.addEventListener('click', function () {
+        if (display.textContent === 'ERROR') return;
         if (/[-+*/]/.test(display.textContent.slice(-1))) return;
-        if (/[-+*/]/.test(display.textContent)) {
-            num2 = +display.textContent.split(/[-+*/]/)[1];
-            num1 = operate(operatorFunctions[operator], num1, num2);
-            console.log(num2, num1);
-            display.textContent = num1 + operation.textContent;
+        if (/[-+*/]/.test(display.textContent.slice(1))) {
+            runCalc(operation);
         } else {
             num1 = +display.textContent;
             display.textContent += operation.textContent;
@@ -65,14 +70,54 @@ operations.forEach(function (operation) {
     });
 });
 
-const equal = document.querySelector('#equal');
-equal.addEventListener('click', function (e) {
-    if (display.textContent.slice(-1).match(/[-+*/]/)) return;
-    num2 = +display.textContent.split(/[-+*/]/)[1];
-    display.textContent = operate(operatorFunctions[operator], num1, num2);
+const equal = document.querySelector('#Enter');
+equal.addEventListener('click', function () {
+    if (/[-+*/]/.test(display.textContent.slice(-1))) return;
+    if (/[-+*/]/.test(display.textContent.slice(1))) {
+        runCalc();
+        refreshNumCheck = display.textContent;
+    }
 })
 
-const clear = document.querySelector('#clear');
+function runCalc(operation = '') {
+    num2 = +display.textContent.split(/[-+*/]/).slice(-1);
+    if (num2 === 0 && operator === 'divide') {
+        display.textContent = 'ERROR';
+    }
+    else {
+        display.textContent = operate(operatorFunctions[operator], num1, num2);
+        num1 = +display.textContent;
+        display.textContent = Math.round((+display.textContent + Number.EPSILON) * 1000) / 1000;
+        if (operation !== '') display.textContent = display.textContent + operation.textContent;
+    }
+}
+
+const clear = document.querySelector('#Backspace');
 clear.addEventListener('click', function (e) {
     display.textContent = '0';
+})
+
+const del = document.querySelector('#Delete');
+del.addEventListener('click', function () {
+    if (display.textContent.length === 1) display.textContent = '0';
+    else display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+})
+
+const percent = document.querySelector('#percent');
+percent.addEventListener('click', function () {
+    if (/[-+*/]/.test(display.textContent.slice(-1))) return;
+    if (display.textContent === '0' || display.textContent === 'ERROR' || refreshNumCheck === display.textContent) return;
+    const stringArr = display.textContent.split(/([-+*/])/);
+    stringArr[stringArr.length - 1] /= 100;
+    display.textContent = stringArr.join('');
+})
+
+window.addEventListener('keydown', function (e) {
+    if (/[0%\-+*/.]/.test(e.key)) {
+        const operations = document.querySelector(`div[data-key="${e.key}"]`);
+        operations.click();
+    } else {
+        const number1_9 = document.querySelector(`div[id="${e.key}"]`);
+        number1_9.click();
+    }
 })
